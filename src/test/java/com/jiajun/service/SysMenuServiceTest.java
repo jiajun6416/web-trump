04f74815d0c5c4f200a1c83dee253e49517446cd
jerry <jiajun6416@gmail.com>
2017-07-10 00:45:01 +0800

    ztree

---
+++ src/test/java/com/jiajun/service/SysMenuServiceTest.java
@@ -0,0 +1,23 @@
+package com.jiajun.service;
+
+
+import java.util.List;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.Assert;
+
+import com.jiajun.base.BaseTest;
+import com.jiajun.pojo.ZtreeNode;
+
+public class SysMenuServiceTest  extends BaseTest{
+
+	@Autowired
+	private SysRoleService sysRoleService;
+
+	@Test
+	public void getCheckTreeNodeByTypeTest() throws Exception {
+		List<ZtreeNode> nodes = sysRoleService.getCheckTreeNodeByType(1);
+		Assert.notEmpty(nodes);
+	}
+}
